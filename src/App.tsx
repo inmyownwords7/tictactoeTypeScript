bc502c389c234bf7502dea1c0c41281e3f8e85d1
inmyownwords7 <andrew.j.lansangan@gmail.com>
2024-12-20 14:39:35 -0500

    Tic Tac Toe TypeScript

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,27 @@
-import { useState } from 'react'
+import {useState} from 'react'
 import Player from './components/Player.tsx'
 import GameBoard from "./components/GameBoard.tsx"
+import "../public/bg-pattern-dark.png"
+import "../public/game-logo.png"
+import "../public/bg-pattern.png"
 import './App.css'
 
 function App() {
+    const [activePlayer, setActivePlayer] = useState("X");
+
+    function handleUpdate() {
+        setActivePlayer((selectedActivePlayer: string) => selectedActivePlayer === "X" ? "O" : "X")
+
+    }
+
     return (
         <main>
             <div id="game-container">
-                <ol id="players">
-                    <Player initialName="Player 1" symbol="X" />
-                    <Player initialName="Player 2" symbol="O" />
+                <ol id="players" className="highlight-player">
+                    <Player initialName="Player 1" symbol="X" isActive={activePlayer === "X"} />
+                    <Player initialName="Player 2" symbol="O" isActive={activePlayer === "O"} />
                 </ol>
-              <GameBoard />
+                <GameBoard onSelectSquare={handleUpdate} activePlayerSymbol={activePlayer} />
             </div>
             LOG
         </main>
--- src/components/GameBoard.tsx
+++ src/components/GameBoard.tsx
@@ -6,16 +6,17 @@ const initialGameBoard: Array<Array<string | null>> = [
     [null, null, null],
 ];
 
-export default function GameBoard() {
+export default function GameBoard({ onSelectSquare, activePlayerSymbol}) {
     const [gameBoard, setGameBoard] = useState(initialGameBoard);
 
     // Select the squares with indexes
     function handleGameBoardChange(rowIndex: number, columnIndex: number) {
         setGameBoard((prevState) => {
             const updatedGameBoard = prevState.map((innerArray) => [...innerArray]); // Create a deep copy
-            updatedGameBoard[rowIndex][columnIndex] = "X"; // Update the specific cell
+            updatedGameBoard[rowIndex][columnIndex] = activePlayerSymbol; // Update the specific cell
             return updatedGameBoard;
         });
+        onSelectSquare();
     }
 
     return (
